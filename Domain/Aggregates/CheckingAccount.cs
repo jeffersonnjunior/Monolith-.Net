dfa067a1d3f0838509bb6ba19833cbbef46f7cfd
Jefferson Junior <jeffcwb1302jr@gmail.com>
2026-01-22 10:39:43 -0300

    feat: add base aggregates and exceptions

---
+++ Domain/Aggregates/CheckingAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace MyBank.Core.Aggregates.AccountAggregate;
+
+public class CheckingAccount
+{
+    public Guid Id { get; private set; }
+    public Guid CustomerId { get; private set; }
+    public decimal Balance { get; private set; }
+
+    protected CheckingAccount() { }
+
+    public CheckingAccount(Guid customerId, decimal initialBalance)
+    {
+        if (initialBalance < 0) throw new DomainException("Saldo inicial inválido.");
+        Id = Guid.NewGuid();
+        CustomerId = customerId;
+        Balance = initialBalance;
+    }
+
+    public void Debit(decimal amount)
+    {
+        if (amount <= 0)
+           throw new DomainException("Valor inválido.");
+
+
+        if (Balance < amount)
+            throw new DomainException("Saldo insuficiente.");
+
+        Balance -= amount;
+    }
+}
